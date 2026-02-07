a1baba9de03aafc1ee856ce11c1a2852640c2742
EmilianoRodriguez78 <emiliano16rod@gmail.com>
2026-02-06 22:57:06 -0600

    Update 12-log_stats.py

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,29 +1,31 @@
 #!/usr/bin/env python3
 """
-Módulo que proporciona estadísticas sobre los logs de Nginx almacenados en MongoDB.
+Script que proporciona estadísticas sobre los logs de Nginx en MongoDB.
 """
 from pymongo import MongoClient
 
 
 def log_stats():
     """
-    Muestra estadísticas sobre la colección nginx en la base de datos logs.
+    Imprime estadísticas de la colección nginx en la base de datos logs.
     """
+    # Conexión al cliente de MongoDB
     client = MongoClient('mongodb://127.0.0.1:27017')
+    # Acceso a la base de datos 'logs' y la colección 'nginx'
     nginx_collection = client.logs.nginx
 
-    # Conteo total de logs
+    # 1. Obtener el número total de documentos
     total_logs = nginx_collection.count_documents({})
     print(f"{total_logs} logs")
 
-    # Estadísticas por métodos
+    # 2. Mostrar conteo por métodos específicos
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
         count = nginx_collection.count_documents({"method": method})
         print(f"\tmethod {method}: {count}")
 
-    # Conteo de status check (GET a /status)
+    # 3. Conteo de documentos con method=GET y path=/status
     status_check = nginx_collection.count_documents(
         {"method": "GET", "path": "/status"}
     )
@@ -32,3 +34,4 @@ def log_stats():
 
 if __name__ == "__main__":
     log_stats()
+    

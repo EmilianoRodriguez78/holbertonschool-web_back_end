13ed48bf2efc976eb203590888d853031466ae2e
EmilianoRodriguez78 <emiliano16rod@gmail.com>
2026-05-06 23:58:44 -0600

    Create 10-div.sql
    Safe divide

---
+++ MySQL_Advanced/10-div.sql
@@ -0,0 +1,17 @@
+-- Create a function SafeDiv that divides two integers safely
+-- Returns a / b or 0 if b is equal to 0
+DELIMITER //
+CREATE FUNCTION SafeDiv(
+    a INT,
+    b INT
+)
+RETURNS FLOAT
+DETERMINISTIC
+BEGIN
+    IF b = 0 THEN
+        RETURN 0;
+    END IF;
+    RETURN (a / b);
+END;
+//
+DELIMITER ;
